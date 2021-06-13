42621f01a0b5daeca11312831d4ad7e3dc9452e6
dbto1970 <donbartojunior@gmail.com>
2021-06-13 13:08:08 -0400

    initial commit

---
+++ js/radioApp.js
@@ -0,0 +1,70 @@
+// Date and Time
+function updateTime() {    
+    const today = new Date();
+    var displayTime = today.toLocaleTimeString();
+    var updateTimeDisplay = setInterval(dateTimeClock, 1000);
+    
+    function dateTimeClock() {
+        var upDate = new Date();
+        var displayTime = upDate.toLocaleTimeString();
+        document.getElementById("date-time").innerHTML = today.toDateString() + " | " + displayTime;
+    }
+}
+updateTime();
+
+
+
+const radioPlayer = document.querySelector('#radioPlayer');
+const wtmdBtn = document.querySelector('#wtmd');
+const wyprBtn = document.querySelector('#wypr');
+const wnrnBtn = document.querySelector('#wnrn');
+const phishBtn = document.querySelector('#livePhish');
+const nowPlaying = document.querySelector('#nowPlaying');
+radioPlayer.volume = 0.7; //set initial audio player volume to 0.7
+wyprBtn.addEventListener('click', changeStation2);
+wtmdBtn.addEventListener('click', changeStation1);
+wnrnBtn.addEventListener('click', changeStation3);
+phishBtn.addEventListener('click', changeStation4);
+
+function changeStation1() {
+    radioPlayer.src = "https://wtmd-ice.streamguys1.com/wtmd.mp3";
+    radioPlayer.type = "audio/mpeg";
+    radioPlayer.setAttribute('autoplay', true);
+    wtmdBtn.classList.add("activeBtn");
+    wyprBtn.classList.remove("activeBtn");
+    wnrnBtn.classList.remove("activeBtn");
+    phishBtn.classList.remove("activeBtn");
+    
+}
+
+function changeStation2() {
+    radioPlayer.src = "http://playerservices.streamtheworld.com/api/livestream-redirect/WYPR_HD1.mp3";
+    radioPlayer.type = "audio/mp4";
+    radioPlayer.setAttribute('autoplay', true);
+    wyprBtn.classList.add("activeBtn");
+    wtmdBtn.classList.remove("activeBtn");
+    wnrnBtn.classList.remove("activeBtn");
+    phishBtn.classList.remove("activeBtn");
+}
+
+function changeStation3() {
+    radioPlayer.src = "https://22113.live.streamtheworld.com/WNRNFM_128.mp3";
+    radioPlayer.type = "audio/mpeg";
+    radioPlayer.setAttribute('autoplay', true);
+    wnrnBtn.classList.add("activeBtn");
+    wyprBtn.classList.remove("activeBtn");
+    wtmdBtn.classList.remove("activeBtn");
+    phishBtn.classList.remove("activeBtn");
+}
+
+function changeStation4() {
+    radioPlayer.src = "https://radio.nugs.net/livephish";
+    radioPlayer.type = "audio/mpeg";
+    radioPlayer.setAttribute('autoplay', true);
+    phishBtn.classList.add("activeBtn");
+    wyprBtn.classList.remove("activeBtn");
+    wnrnBtn.classList.remove("activeBtn");
+    wtmdBtn.classList.remove("activeBtn");
+}
+
+document.onload(changeStation1());
